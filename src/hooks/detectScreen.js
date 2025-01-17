d8c9d7739b73aa9fad3f1e5d01ae23f19b95d599
Anand <rishusingh9369@gmail.com>
2025-01-17 11:57:22 +0530

    bugged fix

--- src/hooks/detectScreen.js
+++ src/hooks/detectScreen.js
@@ -1,8 +1,18 @@
 "use client"
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function useSmallScreen() {
-    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 768);
+    const [isClient, setIsClient] = useState(false)
+
+    const [isSmallScreen, setIsSmallScreen] = useState();
+
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+
+    useEffect(() => {
+        isClient && setIsSmallScreen(window.innerWidth <= 768)
+    }, [isClient])
 
     useEffect(() => {
         const handleResize = () => {
@@ -12,6 +22,7 @@ function useSmallScreen() {
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
     }, []);
+
 
     return isSmallScreen;
 }
