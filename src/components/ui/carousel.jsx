9b27e6a773e62812deaf307671fb159355c4a5fb
Anand <rishusingh9369@gmail.com>
2025-04-08 11:37:24 +0530

    carousel button added

--- src/components/ui/carousel.jsx
+++ src/components/ui/carousel.jsx
@@ -164,61 +164,61 @@ const CarouselItem = React.forwardRef(({ className,registerVideoRef,children, ..
 });
 CarouselItem.displayName = "CarouselItem";
 
-// const CarouselPrevious = React.forwardRef(
-//   ({ className, variant = "outline", size = "icon", ...props }, ref) => {
-//     const { orientation, scrollPrev, canScrollPrev } = useCarousel();
+const CarouselPrevious = React.forwardRef(
+  ({ className, variant = "outline", size = "icon", ...props }, ref) => {
+    const { orientation, scrollPrev, canScrollPrev } = useCarousel();
 
-//     return (
-//       <Button
-//         ref={ref}
-//         variant={variant}
-//         size={size}
-//         className={cn(
-//           "absolute  h-8 w-8 rounded-full bg-white text-black",
-//           orientation === "horizontal"
-//             ? "-left-12 top-1/2 -translate-y-1/2"
-//             : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
-//           className
-//         )}
-//         disabled={!canScrollPrev}
-//         onClick={scrollPrev}
-//         {...props}
-//       >
-//         <ChevronLeft className="h-4 w-4" />
-//         <span className="sr-only">Previous slide</span>
-//       </Button>
-//     );
-//   }
-// );
-// CarouselPrevious.displayName = "CarouselPrevious";
+    return (
+      <Button
+        ref={ref}
+        variant={variant}
+        size={size}
+        className={cn(
+          "absolute  h-8 w-8 rounded-full bg-white text-black",
+          orientation === "horizontal"
+            ? "-left-12 top-1/2 -translate-y-1/2"
+            : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
+          className
+        )}
+        disabled={!canScrollPrev}
+        onClick={scrollPrev}
+        {...props}
+      >
+        <ChevronLeft className="h-4 w-4" />
+        <span className="sr-only">Previous slide</span>
+      </Button>
+    );
+  }
+);
+CarouselPrevious.displayName = "CarouselPrevious";
 
-// const CarouselNext = React.forwardRef(
-//   ({ className, variant = "outline", size = "icon", ...props }, ref) => {
-//     const { orientation, scrollNext, canScrollNext } = useCarousel();
+const CarouselNext = React.forwardRef(
+  ({ className, variant = "outline", size = "icon", ...props }, ref) => {
+    const { orientation, scrollNext, canScrollNext } = useCarousel();
 
-//     return (
-//       <Button
-//         ref={ref}
-//         variant={variant}
-//         size={size}
-//         className={cn(
-//           "absolute h-8 w-8 rounded-full  bg-white text-black",
-//           orientation === "horizontal"
-//             ? "-right-12 top-1/2 -translate-y-1/2"
-//             : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
-//           className
-//         )}
-//         disabled={!canScrollNext}
-//         onClick={scrollNext}
-//         {...props}
-//       >
-//         <ChevronRight className="h-4 w-4" />
-//         <span className="sr-only">Next slide</span>
-//       </Button>
-//     );
-//   }
-// );
-// CarouselNext.displayName = "CarouselNext";
+    return (
+      <Button
+        ref={ref}
+        variant={variant}
+        size={size}
+        className={cn(
+          "absolute h-8 w-8 rounded-full  bg-white text-black",
+          orientation === "horizontal"
+            ? "-right-12 top-1/2 -translate-y-1/2"
+            : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
+          className
+        )}
+        disabled={!canScrollNext}
+        onClick={scrollNext}
+        {...props}
+      >
+        <ChevronRight className="h-4 w-4" />
+        <span className="sr-only">Next slide</span>
+      </Button>
+    );
+  }
+);
+CarouselNext.displayName = "CarouselNext";
 
 export {
   Carousel,
