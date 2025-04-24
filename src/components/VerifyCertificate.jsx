3dc173cfa815a749ba66cacd5c9da3c8e4df54f3
Anand <rishusingh9369@gmail.com>
2025-04-24 14:51:15 +0530

    fixed the bugs of certificate view pdf in mobile

--- src/components/VerifyCertificate.jsx
+++ src/components/VerifyCertificate.jsx
@@ -1,12 +1,19 @@
 "use client"
 import { useVerifyCertificateQuery } from '@/store/Api/course';
-import { usePathname, useSearchParams } from 'next/navigation';
-import React from 'react'
+import { usePathname } from 'next/navigation';
+import React, { useState } from 'react'
 import { SimpleLoader } from './common/LoadingSpinner';
 import { AlertCircle } from 'lucide-react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import useSmallScreen from '@/hooks/detectScreen';
 
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+).toString();
 const VerifyCertificate = () => {
-
+    const isMobile = useSmallScreen();
     const path = usePathname();
     const pathArray = path.split("/")
     const id = pathArray[pathArray.length - 1]
@@ -14,30 +21,46 @@ const VerifyCertificate = () => {
     const { data, isLoading } = useVerifyCertificateQuery(id);
 
     const pdfUrl = data?.data?.certificate;
+
+    const [numPages, setNumPages] = useState();
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
+
     return (
         <div className='h-screen w-screen flex items-center justify-center p-6'>
             {isLoading ? <SimpleLoader /> :
-                <div className='h-auto w-full md:w-1/2 border-[8px] border-[var(--neon-purple)] rounded-lg flex items-center justify-center p-2'>
-                    {pdfUrl ? (
-                            <iframe
-                                src={pdfUrl}
-                                title="Certificate PDF"
-                                className="w-full min-h-60 md:min-h-[540px] rounded-lg border-0"    
+                <>
+                    {pdfUrl && (
+                        <Document
+                            file={pdfUrl}
+                            onLoadSuccess={onDocumentLoadSuccess}
+                            className={'flex flex-col items-center bg-gray-100 rounded-lg p-4 shadow-lg'}
+                            error={
+                                <div className="flex flex-col items-center justify-center gap-4 bg-red-50 border border-red-200 text-red-700 p-6 rounded-xl shadow-md w-78">
+                                    <AlertCircle size={54} />
+                                    <div className="text-center">
+                                        <h2 className="text-xl font-semibold mb-1">{"Oops! Certificate Not Found"}</h2>
+                                        <p className="text-base">
+                                            We couldn&apos;t load the certificate PDF. <br />
+                                            Please check the QR code or try again later.
+                                        </p>
+                                    </div>
+                                </div>
+                            }
+                        >
+                            <Page
+                                pageNumber={pageNumber}
+                                scale={isMobile ? .3 : .6}
+                                renderTextLayer={false}
+                                renderAnnotationLayer={false}
                             />
-                    ) : (
-                        <div className="flex flex-col items-center justify-center gap-4 bg-red-50 border border-red-200 text-red-700 p-6 rounded-xl shadow-md w-96">
-                            <AlertCircle size={54} />
-                            <div className="text-center">
-                                <h2 className="text-xl font-semibold mb-1">Oops! Certificate Not Found</h2>
-                                <p className="text-base">
-                                    We couldnt load the certificate PDF. <br />
-                                    Please check the link or try again later.
-                                </p>
-                            </div>
-                        </div>
-
+                        </Document>
                     )}
-                </div>
+                </>
+
             }
         </div>
     )
--- src/hooks/detectScreen.js
+++ src/hooks/detectScreen.js
@@ -3,12 +3,12 @@ import { useState, useEffect } from "react";
 
 function useSmallScreen() {
     const [isSmallScreen, setIsSmallScreen] = useState(
-        typeof window !== "undefined" && window.innerWidth <= 768
+        typeof window !== "undefined" && window.innerWidth < 768
     );
 
     useEffect(() => {
         const handleResize = () => {
-            setIsSmallScreen(window.innerWidth <= 768);
+            setIsSmallScreen(window.innerWidth < 768);
         };
 
         // Attach resize listener
