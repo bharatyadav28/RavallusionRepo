641b89725215a9a8b5bf1f10fd35479e6b111721
Bharat Yadav <yadavbharat386@gmail.com>
2025-05-23 17:24:29 +0530

    Add Address field

--- src/components/dashboard/PersonalInfoCard.jsx
+++ src/components/dashboard/PersonalInfoCard.jsx
@@ -1,171 +1,230 @@
-'use client';
-import Image from 'next/image';
-import React, { useState } from 'react';
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { OutlinePencil, YellowPencil } from "@/lib/svg_icons";
+import CustomDialog from "../common/CustomDialog";
+import EditInfo from "./EditPersonalInfo";
 import {
-    OutlinePencil,
-    YellowPencil,
-} from '@/lib/svg_icons';
-import CustomDialog from '../common/CustomDialog';
-import EditInfo from './EditPersonalInfo';
-import { useGetUserDetailQuery, useUpdateAvatarMutation, useUpdateMobileMutation, useUpdateNameMutation } from '@/store/Api/auth';
-import { toast } from 'react-toastify';
-import { SimpleLoader } from '../common/LoadingSpinner';
+  useGetUserDetailQuery,
+  useUpdateAddressMutation,
+  useUpdateAvatarMutation,
+  useUpdateMobileMutation,
+  useUpdateNameMutation,
+} from "@/store/Api/auth";
+import { toast } from "react-toastify";
+import { SimpleLoader } from "../common/LoadingSpinner";
 
 const PersonalInfoCard = () => {
-    const { data, isLoading: loading } = useGetUserDetailQuery();
-    const [updateName, { isLoading }] = useUpdateNameMutation();
-    const [updateMobile, { isLoading: isLoadingMobile }] = useUpdateMobileMutation();
-    const [updateAvatar, { isLoading: isLoadingAvatar }] = useUpdateAvatarMutation();
+  const { data, isLoading: loading } = useGetUserDetailQuery();
+  const [updateName, { isLoading }] = useUpdateNameMutation();
+  const [updateMobile, { isLoading: isLoadingMobile }] =
+    useUpdateMobileMutation();
+  const [updateAddress, { isLoading: isLoadingAddress }] =
+    useUpdateAddressMutation();
+  const [updateAvatar, { isLoading: isLoadingAvatar }] =
+    useUpdateAvatarMutation();
 
-    const userName = data?.data?.user?.name || "Ravallusion";
-    const email = data?.data?.user?.email || "NA";
-    const avatar = data?.data?.user?.avatar || '/profilepic.jpeg';
-    const mobileNumber = data?.data?.user?.mobile || 'NA';
+  const userName = data?.data?.user?.name || "Ravallusion";
+  const email = data?.data?.user?.email || "NA";
+  const avatar = data?.data?.user?.avatar || "/profilepic.jpeg";
+  const mobileNumber = data?.data?.user?.mobile || "NA";
+  //   const addressData = data?.data?.user?.address || "NA";
 
-    const [name, setName] = useState(userName);
-    const [phone, setPhone] = useState(mobileNumber);
+  console.log("User: ", data?.data?.user, data?.data?.user?.address);
 
-    const [isOpenName, setIsOpenName] = useState(false);
-    const [isOpenPhone, setIsOpenPhone] = useState(false);
+  const [name, setName] = useState(userName);
+  const [phone, setPhone] = useState(mobileNumber);
+  const [address, setAddress] = useState("");
 
-    // Function to handle saving updated name or phone
-    const handleSave = (field, value) => {
-        if (field === 'name') setName(value);
-        if (field === 'phone') setPhone(value);
-    };
+  const [isOpenName, setIsOpenName] = useState(false);
+  const [isOpenPhone, setIsOpenPhone] = useState(false);
+  const [isOpenAddress, setIsOpenAddress] = useState(false);
 
-    const handleImageChange = async (e) => {
-        const file = e.target.files[0];
-        if (!file) return;
+  // Function to handle saving updated name or phone
+  const handleSave = (field, value) => {
+    if (field === "name") setName(value);
+    if (field === "phone") setPhone(value);
+    if (field === "address") setAddress(value);
+  };
 
-        const formData = new FormData();
-        formData.append("file", file);
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
 
-        try {
-            const res = await updateAvatar(formData).unwrap();
-        } catch (error) {
-            console.log(error);
-            toast.error(error?.data?.message || "Failed to upload image");
-        }
-    };
+    const formData = new FormData();
+    formData.append("file", file);
 
-    const handleUpdateName = async (value) => {
-        handleSave('name', value);
-        try {
-            const res = await updateName({ name: value }).unwrap();
-
-        } catch (error) {
-            console.log("error while Updating name", error)
-            toast.error(error?.data?.message);
-        }
-        finally {
-            setIsOpenName(false);
-        }
+    try {
+      const res = await updateAvatar(formData).unwrap();
+    } catch (error) {
+      console.log(error);
+      toast.error(error?.data?.message || "Failed to upload image");
     }
-    const handleUpdateMobile = async (value) => {
-        handleSave('phone', value);
-        try {
-            const res = await updateMobile({ mobile: value }).unwrap();
-        } catch (error) {
-            console.log("error while Updating phone", error)
-            toast.error(error?.data?.message);
-        }
-        finally {
-            setIsOpenPhone(false);
-        }
+  };
+
+  const handleUpdateName = async (value) => {
+    handleSave("name", value);
+    try {
+      const res = await updateName({ name: value }).unwrap();
+    } catch (error) {
+      console.log("error while Updating name", error);
+      toast.error(error?.data?.message);
+    } finally {
+      setIsOpenName(false);
     }
-    
-    return ( loading ? <div className='flex items-center justify-center min-h-[60vh]'> <SimpleLoader /></div> :
-        <div
-            className=" w-full z-20"
-        >
-            <div className='pt-4 lg:pt-0'>
-                <div className="flex items-center justify-between">
-                    <h1 className="text-lg font-semibold">Personal Information</h1>
-                </div>
+  };
+  const handleUpdateMobile = async (value) => {
+    handleSave("phone", value);
+    try {
+      const res = await updateMobile({ mobile: value }).unwrap();
+    } catch (error) {
+      console.log("error while Updating phone", error);
+      toast.error(error?.data?.message);
+    } finally {
+      setIsOpenPhone(false);
+    }
+  };
 
-                <div className="p-5">
-                    <div className="flex items-center justify-center">
+  const handleUpdateAddress = async (value) => {
+    handleSave("address", value);
+    try {
+      const res = await updateAddress({ address: value }).unwrap();
+    } catch (error) {
+      console.log("error while Updating phone", error);
+      toast.error(error?.data?.message);
+    } finally {
+      setIsOpenAddress(false);
+    }
+  };
 
-                        <div className="relative w-32 h-32 rounded-full">
-                            {isLoadingAvatar ? <SimpleLoader /> :
-                                <Image
-                                    src={avatar || '/profilepic.jpeg'}
-                                    alt="profilepic"
-                                    fill
-                                    style={{ objectFit: 'cover', borderRadius: '50%' }}
-                                />
-                            }
+  const user = data?.data?.user;
+  useEffect(() => {
+    if (user) {
+      setAddress(user?.address);
+    }
+  }, [user]);
 
-                            <div className="absolute bottom-1 right-0 cursor-pointer">
-                                <input
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={handleImageChange}
-                                    className="absolute inset-0 opacity-0 cursor-pointer"
-                                />
-                                <OutlinePencil />
-                            </div>
+  return loading ? (
+    <div className="flex items-center justify-center min-h-[60vh]">
+      {" "}
+      <SimpleLoader />
+    </div>
+  ) : (
+    <div className=" w-full z-20">
+      <div className="pt-4 lg:pt-0">
+        <div className="flex items-center justify-between">
+          <h1 className="text-lg font-semibold">Personal Information</h1>
+        </div>
 
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-            <div className='p-4'>
-                <PersonalInfo label="Name" content={userName} onClick={() => setIsOpenName(true)} />
-                <PersonalInfo label="Phone number" content={mobileNumber} onClick={() => setIsOpenPhone(true)} />
-                <PersonalInfo label="Email id" content={email} />
-            </div>
-
-
-            <CustomDialog open={isOpenName} close={() => setIsOpenName(false)}>
-                <EditInfo
-                    isLoading={isLoading}
-                    label="Name"
-                    content={name}
-                    onClick={() => setIsOpenName(false)}
-                    onSave={(value) => {
-                        handleUpdateName(value);
-                    }}
+        <div className="p-5">
+          <div className="flex items-center justify-center">
+            <div className="relative w-32 h-32 rounded-full">
+              {isLoadingAvatar ? (
+                <SimpleLoader />
+              ) : (
+                <Image
+                  src={avatar || "/profilepic.jpeg"}
+                  alt="profilepic"
+                  fill
+                  style={{ objectFit: "cover", borderRadius: "50%" }}
                 />
-            </CustomDialog>
+              )}
 
-            <CustomDialog open={isOpenPhone} close={() => setIsOpenPhone(false)}>
-                <EditInfo
-                    isLoading={isLoadingMobile}
-                    label="Phone"
-                    type='text'
-                    content={phone}
-                    onClick={() => setIsOpenPhone(false)}
-                    onSave={(value) => {
-                        handleUpdateMobile(value);
-                    }}
+              <div className="absolute bottom-1 right-0 cursor-pointer">
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="absolute inset-0 opacity-0 cursor-pointer"
                 />
-            </CustomDialog>
-        </div >
-    );
+                <OutlinePencil />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="p-4">
+        <PersonalInfo
+          label="Name"
+          content={userName}
+          onClick={() => setIsOpenName(true)}
+        />
+        <PersonalInfo
+          label="Phone number"
+          content={mobileNumber}
+          onClick={() => setIsOpenPhone(true)}
+        />
+        <PersonalInfo label="Email id" content={email} />
+        <PersonalInfo
+          label="Address"
+          content={address}
+          onClick={() => setIsOpenAddress(true)}
+        />
+      </div>
+
+      <CustomDialog open={isOpenName} close={() => setIsOpenName(false)}>
+        <EditInfo
+          isLoading={isLoading}
+          label="Name"
+          content={name}
+          onClick={() => setIsOpenName(false)}
+          onSave={(value) => {
+            handleUpdateName(value);
+          }}
+        />
+      </CustomDialog>
+
+      <CustomDialog open={isOpenPhone} close={() => setIsOpenPhone(false)}>
+        <EditInfo
+          isLoading={isLoadingMobile}
+          label="Phone"
+          type="text"
+          content={phone}
+          onClick={() => setIsOpenPhone(false)}
+          onSave={(value) => {
+            handleUpdateMobile(value);
+          }}
+        />
+      </CustomDialog>
+
+      <CustomDialog open={isOpenAddress} close={() => setIsOpenPhone(false)}>
+        <EditInfo
+          isLoading={isLoadingAddress}
+          label="Address"
+          type="text"
+          content={address}
+          onClick={() => setIsOpenAddress(false)}
+          onSave={(value) => {
+            handleUpdateAddress(value);
+          }}
+        />
+      </CustomDialog>
+    </div>
+  );
 };
 
 const PersonalInfo = ({ label, content, onClick }) => {
-    return (
-        <div className="py-1">
-            <label htmlFor="name" className="text-[13px] ml-1 text-gray-400 important">
-                {label}
-            </label>
-            <div className="flex items-center justify-between relative">
-                <p className="text-sm font-medium border border-gray-500 p-4 w-full rounded-xl ">{content}</p>
-                {label !== 'Email id' && (
-                    <div className="cursor-pointer absolute right-3" onClick={onClick}>
-                        <YellowPencil />
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className="py-1">
+      <label
+        htmlFor="name"
+        className="text-[13px] ml-1 text-gray-400 important"
+      >
+        {label}
+      </label>
+      <div className="flex items-center justify-between relative">
+        <p className="text-sm font-medium border border-gray-500 p-4 w-full rounded-xl ">
+          {content}
+        </p>
+        {label !== "Email id" && (
+          <div className="cursor-pointer absolute right-3" onClick={onClick}>
+            <YellowPencil />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
-
-
 
 export default PersonalInfoCard;
--- src/store/Api/auth.js
+++ src/store/Api/auth.js
@@ -1,96 +1,106 @@
-import { Logout } from '@/lib/svg_icons';
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Logout } from "@/lib/svg_icons";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
-    reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({ baseUrl: '/api/v1/' }),
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/api/v1/" }),
 
-    tagTypes: ['user'],
-    endpoints: (builder) => ({
-        signin: builder.mutation({
-            query: (body) => ({
-                url: "user/signin",
-                method: "POST",
-                credentials: "include",
-                body,
-            })
-        }),
-        hasSubscription: builder.query({
-            query: (userId) => `order/has-subscription/${userId}`
-        }),
-        verifyUser: builder.mutation({
-            query: (body) => ({
-                url: "user/verify-user",
-                method: "POST",
-                credentials: "include",
-                body,
-            })
-        }),
-        logout: builder.mutation({
-            query: () => ({
-                url: "user/logout",
-                method: "DELETE",
-                credentials: "include",
-            })
-        }),
-        switchDevice: builder.mutation({
-            query: () => ({
-                url: "user/switch-device",
-                method: "POST",
-            })
-        }),
-        getUserDetail: builder.query({
-            query: () => "user/send-me",
-            providesTags: ["user"]
-        }),
+  tagTypes: ["user"],
+  endpoints: (builder) => ({
+    signin: builder.mutation({
+      query: (body) => ({
+        url: "user/signin",
+        method: "POST",
+        credentials: "include",
+        body,
+      }),
+    }),
+    hasSubscription: builder.query({
+      query: (userId) => `order/has-subscription/${userId}`,
+    }),
+    verifyUser: builder.mutation({
+      query: (body) => ({
+        url: "user/verify-user",
+        method: "POST",
+        credentials: "include",
+        body,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "user/logout",
+        method: "DELETE",
+        credentials: "include",
+      }),
+    }),
+    switchDevice: builder.mutation({
+      query: () => ({
+        url: "user/switch-device",
+        method: "POST",
+      }),
+    }),
+    getUserDetail: builder.query({
+      query: () => "user/send-me",
+      providesTags: ["user"],
+    }),
 
-        updateName: builder.mutation({
-            query: (body) => ({
-                url: "user/name",
-                method: "PUT",
-                body
-            }),
-            invalidatesTags: ["user"]
-        }),
+    updateName: builder.mutation({
+      query: (body) => ({
+        url: "user/name",
+        method: "PUT",
+        body,
+      }),
+      invalidatesTags: ["user"],
+    }),
 
-        updateMobile: builder.mutation({
-            query: (body) => ({
-                url: "user/mobile",
-                method: "PUT",
-                body
-            }),
-            invalidatesTags: ["user"]
-        }),
-        updateAvatar: builder.mutation({
-            query: (body) => ({
-                url: "user/avatar",
-                method: "PUT",
-                body
-            }),
-            invalidatesTags: ["user"]
-        }),
-        deleteAccount: builder.mutation({
-            query: () => ({
-                url: "user/delete-account",
-                method: "DELETE",
-            }),
-        }),
-        getActivePaymentGateway: builder.query({
-            query: () => "app-config/active-gateways",
-        })
-    })
-})
+    updateMobile: builder.mutation({
+      query: (body) => ({
+        url: "user/mobile",
+        method: "PUT",
+        body,
+      }),
+      invalidatesTags: ["user"],
+    }),
+    updateAddress: builder.mutation({
+      query: (body) => ({
+        url: "user/address",
+        method: "PUT",
+        body,
+      }),
+      invalidatesTags: ["user"],
+    }),
+    updateAvatar: builder.mutation({
+      query: (body) => ({
+        url: "user/avatar",
+        method: "PUT",
+        body,
+      }),
+      invalidatesTags: ["user"],
+    }),
+    deleteAccount: builder.mutation({
+      query: () => ({
+        url: "user/delete-account",
+        method: "DELETE",
+      }),
+    }),
+    getActivePaymentGateway: builder.query({
+      query: () => "app-config/active-gateways",
+    }),
+  }),
+});
 
-
-export const { useDeleteAccountMutation, useSigninMutation,
-    useUpdateMobileMutation,
-    useUpdateNameMutation,
-    useUpdateAvatarMutation,
-    useVerifyUserMutation,
-    useLogoutMutation,
-    useSwitchDeviceMutation,
-    useLazyHasSubscriptionQuery,
-    useGetUserDetailQuery,
-    useHasSubscriptionQuery,
-    useGetActivePaymentGatewayQuery,
+export const {
+  useDeleteAccountMutation,
+  useSigninMutation,
+  useUpdateMobileMutation,
+  useUpdateNameMutation,
+  useUpdateAvatarMutation,
+  useVerifyUserMutation,
+  useLogoutMutation,
+  useSwitchDeviceMutation,
+  useLazyHasSubscriptionQuery,
+  useGetUserDetailQuery,
+  useHasSubscriptionQuery,
+  useGetActivePaymentGatewayQuery,
+  useUpdateAddressMutation,
 } = authApi;
