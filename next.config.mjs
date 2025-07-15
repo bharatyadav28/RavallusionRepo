c0fc068fb93dcb342d6fa61d5194cc93bdaec4d4
Bharat Yadav <yadavbharat386@gmail.com>
2025-07-15 13:33:21 +0530

    Update dev backend

--- next.config.mjs
+++ next.config.mjs
@@ -40,6 +40,7 @@ const nextConfig = {
       {
         source: "/api/:path*",
         destination: "https://api.ravallusion.com/api/:path*",
+        destination: "https://revallusion.onrender.com/api/:path*",
 
         // destination: "http://localhost:4000/api/:path*",
       },
